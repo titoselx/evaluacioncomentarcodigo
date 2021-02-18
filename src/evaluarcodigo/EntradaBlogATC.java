73c1fb2df3eded36562f613d8472efcd373862e2
titoselx <74183026+titoselx@users.noreply.github.com>
2021-02-18 20:41:48 +0100

    Añadidos comentarios javadoc

--- src/evaluarcodigo/EntradaBlogATC.java
+++ src/evaluarcodigo/EntradaBlogATC.java
@@ -6,18 +6,28 @@
 package evaluarcodigo;
 
 /**
- *
- * @author ATcoils
+ *PRUEBA PARA ENTORNOS DE DESARROLLO
+ * @author AARONTITOS
  */
 public class EntradaBlogATC {
-    //separador es el ccarácter que separa ENTRADA DE del
-    //nombre del autor
+    /**
+     * Separador es el carácter que separa ENTRADA del nombre del autor
+     */
 
     public static char separador = ':';
     private int id;
     private String texto;
     private String autor;
-
+/**
+ * CONSTRUCTOR DE LA CLASE
+ * recibe el número de entrada, el nombre del autor de la entrada y el texto 
+ * que contiene la entrada. Si el número de entrada es negativo,
+ * lanza una excepción.
+ * @param id
+ * @param autor
+ * @param texto
+ * @throws IllegalArgumentException 
+ */
     //Constructor de la clase. recibe el número de entrada, el nombre del autor
     //de la entrada y el texto que contiene la entrada. Si el número de entrada
     //es negativo, lanza una excepción.
@@ -37,28 +47,41 @@ public class EntradaBlogATC {
         cad += "\n " + texto;
         return cad;
     }
-
-    //Devuelve el número de la entrada
+/**
+ * Devuelve el número de la entrada
+ * @return 
+ */
     public int getId() {
         return this.id;
     }
-
-    //devuelve el texto completo de la entrada
+/**
+ * devuelve el texto completo de la entrada
+ * @return 
+ */
     public String getTexto() {
         return this.texto;
     }
-
-    //devuelve el nombre del autor de la entrada en mayúsculas
+/**
+ * devuelve el nombre del autor de la entrada en mayúsculas
+ * @return 
+ */
+    
     public String getAutor() {
         return this.autor.toUpperCase();
     }
-
-    //devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+/**
+ * devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+ * @return 
+ */
+    
     public String devuelveAutor() {
         return this.autor;
     }
-
-    //No tiene porqué tener argumentos.
+/**
+ * No tiene porqué tener argumentos.
+ * @param args 
+ */
+    
     public static void main(String[] args) {
         //Modificar.
         EntradaBlogATC e1 = new EntradaBlogATC(3, "Aaron Titos", "Últimas noticias, ¡QUIERO APROBAR LA ASIGNATURA!");
